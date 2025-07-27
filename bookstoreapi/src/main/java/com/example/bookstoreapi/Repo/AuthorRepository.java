0e14da4ae56b493c1037ca501c3c8240335525e7
Srinivas-jella <132424409+Srinivas-jella@users.noreply.github.com>
2025-07-27 19:32:45 +0530

    Delete bookstoreapi directory

--- bookstoreapi/src/main/java/com/example/bookstoreapi/Repo/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bookstoreapi.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.bookstoreapi.dto.Author;
-
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-
-}
